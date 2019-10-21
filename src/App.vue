3c5c586d370a6a720c2b5e800bcb52bf499a7c85
mewlips <mewlips@muon.co>
2019-10-21 14:32:52 +0900

    Fix unit tests

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,10 @@
   <v-app>
     <v-app-bar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>뮤온</span>
+        <span class="font-weight-light">MUON</span>
       </v-toolbar-title>
-      <v-spacer></v-spacer>
+      <v-spacer />
       <v-btn
         text
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
@@ -16,22 +16,43 @@
     </v-app-bar>
 
     <v-content>
-      <HelloWorld />
+      <hello-world />
     </v-content>
+
+    <v-footer
+      color="primary lighten-1"
+      app
+    >
+      <v-row justify="left">
+        <div>
+          뮤온(주) | 대표이사: 박홍석
+        </div>
+        <div>
+          서울시 영등포구 양평로21가길 19 선유도우림라이온스밸리 B동 509호
+        </div>
+        <div>
+          사업자등록번호: xxx-xx-xxxxx
+        </div>
+      </v-row>
+      <v-row justify="center">
+        &copy; Copyright {{ new Date().getFullYear() }}, MUON Corp. All Rights
+        Reserved
+      </v-row>
+    </v-footer>
   </v-app>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+  import Vue from 'vue'
+  import HelloWorld from './components/HelloWorld.vue'
 
-export default Vue.extend({
-  name: "App",
-  components: {
-    HelloWorld
-  },
-  data: () => ({
-    //
+  export default Vue.extend({
+    name: 'App',
+    components: {
+      HelloWorld,
+    },
+    data: () => ({
+      //
+    }),
   })
-});
 </script>
--- tests/e2e/.eslintrc.js
+++ tests/e2e/.eslintrc.js
@@ -6,5 +6,6 @@ module.exports = {
   },
   rules: {
     strict: "off"
-  }
+  },
+  extends: "vuetify"
 };
